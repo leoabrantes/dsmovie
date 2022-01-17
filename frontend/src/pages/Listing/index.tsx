88ddc0fb21dea528c1dc9584cbbd0692c9a467d8
Leo Abrantes <leoabrantesmelo@gmail.com>
2022-01-16 22:00:51 -0300

    Pagination

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -24,11 +24,8 @@ function Listing() {
 
     });
 
-
-
-
     useEffect(() => {
-        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}&sort=title`)
+        axios.get(`${BASE_URL}/movies?size=12&page=${pageNumber}`)
             .then(response => {
                 const data = response.data as MoviePage;
                 setPage(data);
@@ -36,12 +33,14 @@ function Listing() {
 
     }, [pageNumber]);
 
-
+    const handlePageChange = (newPageNumber : number) => {
+        setPageNumber(newPageNumber);
+    }
 
     return (
         <>
 
-            <Pagination />
+            <Pagination page={page} onChange={handlePageChange} />
 
             <div className="container">
                 <div className="row">
