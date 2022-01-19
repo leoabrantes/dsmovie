1979feed648df4b2d876a4561539227c53af3b16
Leo Abrantes <leoabrantesmelo@gmail.com>
2022-01-19 13:08:23 -0300

    Changing github address

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -12,7 +12,7 @@ function Navbar(){
                     <a href="https://github.com/leoabrantes" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
-                            <p className="dsmovie-contact-link">/devsuperior</p>
+                            <p className="dsmovie-contact-link">/leoabrantes</p>
                         </div>
                     </a>
                 </div>
